e8c786f38d4d55df47cf4db17150b8a5d571afdb
ankul-in <ankulin@outlook.com>
2025-12-24 23:52:34 +0530

    1

---
+++ twoDefaultParameter.cpp
@@ -0,0 +1,23 @@
+//twoDefaultParameter
+
+#include <iostream>
+
+using namespace std;
+
+int Add(int a=10,int b=20){
+    
+    return (a+b);
+    
+    
+}
+
+int main(){
+    int x=5,y=6,z;
+    z=add(x,y);
+    cout<<"sum="<<z<<endl;
+    z=add(x);
+    cout<<"sum="<<z<<endl;
+    z=add();
+    cout<<"sum="<<z<<endl;
+}
+    
