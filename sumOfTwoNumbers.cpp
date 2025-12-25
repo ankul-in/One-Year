2712038a9bde9989c30986d020202e432e044573
ankul-in <ankulin@outlook.com>
2025-12-25 11:33:15 +0530

    6

---
+++ sumOfTwoNumbers.cpp
@@ -0,0 +1,12 @@
+// https://youtu.be/R9byHM7vzn8
+
+#include <iostream>
+int main()
+{
+    int a, b;
+    std::cout << " input two numbers ";
+    std::cin >> a >> b;
+    int sum = a + b;
+    std::cout << " sum of numbers " << a << " and " << b << " is equals to " << sum;
+    return 0;
+}
