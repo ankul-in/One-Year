1ac6da8c34ff591c7c5a1b6955ce8b6e8cb4a364
ankul-in <ankulin@outlook.com>
2025-12-25 11:35:21 +0530

    12

---
+++ DefaultSort.cpp
@@ -0,0 +1,24 @@
+// https://youtu.be/x_IAtg8ChH8
+
+// default sort of algroithm
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+    int data[] = {
+        32, 71, 12, 45, 26
+
+    };
+
+    sort(data, data + 5);
+    for (int i = 0; i < 5; i++)
+    {
+
+        cout << data[i] << " ";
+    }
+
+    return 0;
+}
