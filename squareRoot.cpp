89b44a295eb20d0ac6faec89ae8ae3b60c9f1881
ankul-in <ankulin@outlook.com>
2025-12-25 11:33:23 +0530

    7

---
+++ squareRoot.cpp
@@ -0,0 +1,16 @@
+// https://youtu.be/R9byHM7vzn8
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cout << "enter number to find its squareroot ";
+    cin >> a;
+    double sqrt_n = sqrt(a);
+    cout << "square root of " << a << " is " << sqrt_n;
+    return 0;
+}
