16d6bfdcd2bf20461686b2cd68f1c074b7565a8d
ankul-in <ankulin@outlook.com>
2025-12-26 23:52:44 +0530

    19

---
+++ const.cpp
@@ -0,0 +1,21 @@
+//https://youtu.be/Z4AfBIzmA1U
+
+//const
+
+#include <iostream>
+
+#include <cmath>
+
+using namespace std;
+const int TWO = 2;
+const double PI = 4.0*atan(1.0);
+
+int main(){
+    int r=10;
+    double peri = TWO*PI*r;
+    
+    cout << "perimeter is "<<peri;
+    
+    return 0;
+    
+}
