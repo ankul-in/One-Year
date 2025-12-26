8683a2eb9ba7424618ff30d9c435d3b678311c8f
ankul-in <ankulin@outlook.com>
2025-12-26 23:52:07 +0530

    17

---
+++ manifestConstants.cpp
@@ -0,0 +1,24 @@
+//https://youtu.be/Z4AfBIzmA1U
+
+//manifest constants
+
+#include <iostream>
+
+#include <cmath>
+
+using namespace std;
+
+#define TWO 2
+#define PI 4.0*atan(1.0)
+
+int main(){
+    int r=10;
+    double peri = TWO*PI*r;
+    
+    cout << "perimeter is "<<peri;
+    
+    return 0;
+    
+    
+    
+}
