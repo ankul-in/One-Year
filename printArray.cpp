349a4ff9756d1d6712b52a1b930785066b19e15e
ankul-in <ankulin@outlook.com>
2025-12-27 18:51:27 +0530

    24

---
+++ printArray.cpp
@@ -0,0 +1,21 @@
+// https://youtu.be/rDTY6525iGM
+
+// print array cpp
+
+#include <iostream>
+
+int main()
+{
+
+    short age[4];
+
+    age[0] = 23;
+    age[1] = 34;
+    age[2] = 65;
+    age[3] = 74;
+
+    std::cout << age[0] << std::endl;
+    std::cout << age[1] << std::endl;
+    std::cout << age[2] << std::endl;
+    std::cout << age[3] << std::endl;
+}
